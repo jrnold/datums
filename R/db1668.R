f3d064b8df4fff1ffc02a65ed8350dd1af21a94c
jrnold <jeffrey.arnold@gmail.com>
2017-06-10 23:19:49 -0700

    update template and docs

--- R/db1668.R
+++ R/db1668.R
@@ -1,51 +1,30 @@
 #' Table of Magnetic Variation near Bristol on May 23, 1668
 #'
-#' 
 #' This is from a note published in the \emph{Transactions of the Royal Society} in 1668. The editor, Henry Oldenburg,  printed parts of a letter from "D.B." with five values of the mangetic variation near Bristol, England on May 23, 1668. This letter is notable in that he calculates the "mean of the true Variation", where "mean" refers to the artithmetic mean. It is referenced by Stigler as the first instance in which the term "mean" is used to refer to the arithmetical mean. D.B. reported a mean of 1 degree 27 minutes, rounding down from the arithmetic mean of 1 degree 28 minutes.
 #' 
 #' 
 #' 
-#' 
-#' 
 #'
-#' 
+#' @format
 #' A data frame with 4 columns and 5 rows:
 #' \describe{
-#'  \item{ \code{ sun_altitude }}{ \code{ numeric }. Sun's altitude in degrees }
-#'  \item{ \code{ magnetic_azimuth }}{ \code{ numeric }. Magnetic azimuth in degrees }
-#'  \item{ \code{ sun_azimuth }}{ \code{ numeric }. Sun's azimuth in degrees }
-#'  \item{ \code{ variation }}{ \code{ numeric }. Variation in degrees westerly, between true north (sun azimuth) and magnetic north (magnetic azimuth) }
-#' 
+#'  \item{ \code{ sun_altitude }}{ \code{ numeric }. Sun's altitude in degrees } \item{ \code{ magnetic_azimuth }}{ \code{ numeric }. Magnetic azimuth in degrees } \item{ \code{ sun_azimuth }}{ \code{ numeric }. Sun's azimuth in degrees } \item{ \code{ variation }}{ \code{ numeric }. Variation in degrees westerly, between true north (sun azimuth) and magnetic north (magnetic azimuth) }
 #' }
-#' 
+#' @format
 #' A data frame with 4 columns and 5 rows:
 #' \describe{
-#'  \item{ \code{ sun_altitude }}{ \code{ numeric }. Sun's altitude in degrees }
-#'  \item{ \code{ magnetic_azimuth }}{ \code{ numeric }. Magnetic azimuth in degrees }
-#'  \item{ \code{ sun_azimuth }}{ \code{ numeric }. Sun's azimuth in degrees }
-#'  \item{ \code{ variation }}{ \code{ numeric }. Variation in degrees westerly, between true north (sun azimuth) and magnetic north (magnetic azimuth) }
-#' 
+#'  \item{ \code{ sun_altitude }}{ \code{ numeric }. Sun's altitude in degrees } \item{ \code{ magnetic_azimuth }}{ \code{ numeric }. Magnetic azimuth in degrees } \item{ \code{ sun_azimuth }}{ \code{ numeric }. Sun's azimuth in degrees } \item{ \code{ variation }}{ \code{ numeric }. Variation in degrees westerly, between true north (sun azimuth) and magnetic north (magnetic azimuth) }
 #' }
-#' 
+#' @format
 #' A data frame with 4 columns and 5 rows:
 #' \describe{
-#'  \item{ \code{ sun_altitude }}{ \code{ numeric }. Sun's altitude in degrees }
-#'  \item{ \code{ magnetic_azimuth }}{ \code{ numeric }. Magnetic azimuth in degrees }
-#'  \item{ \code{ sun_azimuth }}{ \code{ numeric }. Sun's azimuth in degrees }
-#'  \item{ \code{ variation }}{ \code{ numeric }. Variation in degrees westerly, between true north (sun azimuth) and magnetic north (magnetic azimuth) }
-#' 
+#'  \item{ \code{ sun_altitude }}{ \code{ numeric }. Sun's altitude in degrees } \item{ \code{ magnetic_azimuth }}{ \code{ numeric }. Magnetic azimuth in degrees } \item{ \code{ sun_azimuth }}{ \code{ numeric }. Sun's azimuth in degrees } \item{ \code{ variation }}{ \code{ numeric }. Variation in degrees westerly, between true north (sun azimuth) and magnetic north (magnetic azimuth) }
 #' }
-#' 
+#' @format
 #' A data frame with 4 columns and 5 rows:
 #' \describe{
-#'  \item{ \code{ sun_altitude }}{ \code{ numeric }. Sun's altitude in degrees }
-#'  \item{ \code{ magnetic_azimuth }}{ \code{ numeric }. Magnetic azimuth in degrees }
-#'  \item{ \code{ sun_azimuth }}{ \code{ numeric }. Sun's azimuth in degrees }
-#'  \item{ \code{ variation }}{ \code{ numeric }. Variation in degrees westerly, between true north (sun azimuth) and magnetic north (magnetic azimuth) }
-#' 
+#'  \item{ \code{ sun_altitude }}{ \code{ numeric }. Sun's altitude in degrees } \item{ \code{ magnetic_azimuth }}{ \code{ numeric }. Magnetic azimuth in degrees } \item{ \code{ sun_azimuth }}{ \code{ numeric }. Sun's azimuth in degrees } \item{ \code{ variation }}{ \code{ numeric }. Variation in degrees westerly, between true north (sun azimuth) and magnetic north (magnetic azimuth) }
 #' }
-#' 
-#'
 #' 
 #' @references
 #' \itemize{
@@ -53,11 +32,8 @@
 #' 
 #' }
 #' 
-#'
-#' 
 #' @source
 #' Stigler, Stephen M. \emph{The Seven Pillars of Statistical Wisdom}
 #' 
-#'
 #' 
 "db1668"
--- R/loterie_draws.R
+++ R/loterie_draws.R
@@ -1,87 +1,42 @@
 #' French Lottery Winning Draws 1758-1834
 #'
-#' 
 #' The winning draws for the French lottery, 1758-1834. Each drawing consisted of drawing five balls without replacement, from a bin of balls numbered 1 to 90. Players could place bets by specifying a set of numbers, which had different names and payoffs based on the number specified.
-#' 
 #' \tabular{rrl}{
-#' 
 #' name \tab number correct \tab payoff \cr
-#' 
 #' quine \tab five \tab 1,000,000 \cr
-#' 
 #' quartene \tab four \tab 75,000 \cr
-#' 
 #' terne \tab three \tab 5,000 \cr
-#' 
 #' ambe \tab two \tab 270 \cr
-#' 
 #' extrait \tab one \tab 15
-#' 
 #' }
-#' 
-#' 
 #' 
 #' 
 #' 
 #'
-#' 
+#' @format
 #' A data frame with 5 columns and 6606 rows:
 #' \describe{
-#'  \item{ \code{ X1 }}{ \code{ integer }. First number }
-#'  \item{ \code{ X2 }}{ \code{ integer }. Second number }
-#'  \item{ \code{ X3 }}{ \code{ integer }. Third number }
-#'  \item{ \code{ X4 }}{ \code{ integer }. Fourth number }
-#'  \item{ \code{ X5 }}{ \code{ integer }. Fifth number }
-#' 
+#'  \item{ \code{ X1 }}{ \code{ integer }. First number } \item{ \code{ X2 }}{ \code{ integer }. Second number } \item{ \code{ X3 }}{ \code{ integer }. Third number } \item{ \code{ X4 }}{ \code{ integer }. Fourth number } \item{ \code{ X5 }}{ \code{ integer }. Fifth number }
 #' }
-#' 
+#' @format
 #' A data frame with 5 columns and 6606 rows:
 #' \describe{
-#'  \item{ \code{ X1 }}{ \code{ integer }. First number }
-#'  \item{ \code{ X2 }}{ \code{ integer }. Second number }
-#'  \item{ \code{ X3 }}{ \code{ integer }. Third number }
-#'  \item{ \code{ X4 }}{ \code{ integer }. Fourth number }
-#'  \item{ \code{ X5 }}{ \code{ integer }. Fifth number }
-#' 
+#'  \item{ \code{ X1 }}{ \code{ integer }. First number } \item{ \code{ X2 }}{ \code{ integer }. Second number } \item{ \code{ X3 }}{ \code{ integer }. Third number } \item{ \code{ X4 }}{ \code{ integer }. Fourth number } \item{ \code{ X5 }}{ \code{ integer }. Fifth number }
 #' }
-#' 
+#' @format
 #' A data frame with 5 columns and 6606 rows:
 #' \describe{
-#'  \item{ \code{ X1 }}{ \code{ integer }. First number }
-#'  \item{ \code{ X2 }}{ \code{ integer }. Second number }
-#'  \item{ \code{ X3 }}{ \code{ integer }. Third number }
-#'  \item{ \code{ X4 }}{ \code{ integer }. Fourth number }
-#'  \item{ \code{ X5 }}{ \code{ integer }. Fifth number }
-#' 
+#'  \item{ \code{ X1 }}{ \code{ integer }. First number } \item{ \code{ X2 }}{ \code{ integer }. Second number } \item{ \code{ X3 }}{ \code{ integer }. Third number } \item{ \code{ X4 }}{ \code{ integer }. Fourth number } \item{ \code{ X5 }}{ \code{ integer }. Fifth number }
 #' }
-#' 
+#' @format
 #' A data frame with 5 columns and 6606 rows:
 #' \describe{
-#'  \item{ \code{ X1 }}{ \code{ integer }. First number }
-#'  \item{ \code{ X2 }}{ \code{ integer }. Second number }
-#'  \item{ \code{ X3 }}{ \code{ integer }. Third number }
-#'  \item{ \code{ X4 }}{ \code{ integer }. Fourth number }
-#'  \item{ \code{ X5 }}{ \code{ integer }. Fifth number }
-#' 
+#'  \item{ \code{ X1 }}{ \code{ integer }. First number } \item{ \code{ X2 }}{ \code{ integer }. Second number } \item{ \code{ X3 }}{ \code{ integer }. Third number } \item{ \code{ X4 }}{ \code{ integer }. Fourth number } \item{ \code{ X5 }}{ \code{ integer }. Fifth number }
 #' }
-#' 
+#' @format
 #' A data frame with 5 columns and 6606 rows:
 #' \describe{
-#'  \item{ \code{ X1 }}{ \code{ integer }. First number }
-#'  \item{ \code{ X2 }}{ \code{ integer }. Second number }
-#'  \item{ \code{ X3 }}{ \code{ integer }. Third number }
-#'  \item{ \code{ X4 }}{ \code{ integer }. Fourth number }
-#'  \item{ \code{ X5 }}{ \code{ integer }. Fifth number }
-#' 
-#' }
-#' 
-#'
-#' 
-#' @references
-#' \itemize{
-#' \item{ Stephen M. Stigler. 2003. \dQuote{Casanova, Bonaparte and the Loterie de France}, \emph{Journal de la Société Française de Statistique} }
-#' \item{ Stigler, Stephen M. \emph{The Seven Pillars of Statistical Wisdom}, p. 166-170. }
-#' 
+#'  \item{ \code{ X1 }}{ \code{ integer }. First number } \item{ \code{ X2 }}{ \code{ integer }. Second number } \item{ \code{ X3 }}{ \code{ integer }. Third number } \item{ \code{ X4 }}{ \code{ integer }. Fourth number } \item{ \code{ X5 }}{ \code{ integer }. Fifth number }
 #' }
 #' 
 #' @references
@@ -90,12 +45,15 @@
 #' \item{ Stigler, Stephen M. \emph{The Seven Pillars of Statistical Wisdom}, p. 166-170. }
 #' 
 #' }
+#' @references
+#' \itemize{
+#' \item{ Stephen M. Stigler. 2003. \dQuote{Casanova, Bonaparte and the Loterie de France}, \emph{Journal de la Société Française de Statistique} }
+#' \item{ Stigler, Stephen M. \emph{The Seven Pillars of Statistical Wisdom}, p. 166-170. }
 #' 
-#'
+#' }
 #' 
 #' @source
 #' \url{http://www.stat.uchicago.edu/~stigler/French_Lottery_Data.html}
 #' 
-#'
 #' 
 "loterie_draws"
