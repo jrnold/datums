66efd0d0873390420622558e126f9e22f579f464
jrnold <jeffrey.arnold@gmail.com>
2018-05-07 15:22:35 -0700

    add note

--- data-raw/cholera.R
+++ data-raw/cholera.R
@@ -1,3 +1,13 @@
+# TODO: right now only uses Table XII reproduced in mastering metrics
+# This does not have populations in each district.
+#
+# Get and combine original tables from https://play.google.com/books/reader?id=-N0_AAAAcAAJ
+#
+# -  Table V. p. 71
+# -  Table VI. p. 73
+# -  Table VII, VIII
+# -  Table XII
+#
 read_csv("data-raw/cholera.csv", na = "",
          col_types = cols(
            sub_district = col_character(),
